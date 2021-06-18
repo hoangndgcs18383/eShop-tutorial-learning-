6b4206ebeeec9b08d09d21578d986d3a25c3ca9f
hoangndgcs18383 <74606110+hoangndgcs18383@users.noreply.github.com>
2021-06-19 05:26:54 +0700

    Add entitirs

---
+++ eShopSolution.Data/EF/eShopDbContext.cs
@@ -0,0 +1,27 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class eShopDbContext : DbContext
+    {
+        public eShopDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products {get; set;}
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<Category> Categorys { get; set; }
+        public DbSet<CategoryTransaction> CategoryTransactions { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductTransaction> ProductTransactions { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}
